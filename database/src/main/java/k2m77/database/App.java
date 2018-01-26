da3622caf0757b7a77d74b0b9773b1a4da4ae268
Huangxm <huangxmwork@qq.com>
2018-01-26 14:39:28 +0800

    create project

---
+++ database/src/main/java/k2m77/database/App.java
@@ -0,0 +1,13 @@
+package k2m77.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
